d88f5788f3f6db7857db763356b0ceedd8bfd442
paragborkar <paragborkar200@gmail.com>
2023-12-23 16:18:09 +0530

    Header Created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from '@/components/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,12 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}>
         <div className='bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25]
          w-[31.25rem] rounded-full -z-10 blur-[10rem] sm:w-[68.5rem]'></div>
           <div className='bg-[#dbd7fb] absolute top-[-1rem] left-[35rem] h-[31.25]
          w-[50rem] rounded-full -z-10 blur-[10rem] sm:w-[68.5rem] md:left-[-33rem] lg:left-[-28rem]
          xl:left-[-15] 2xl:left[-5rem]'></div>
+         <Header/>
         {children}</body>
     </html>
   )

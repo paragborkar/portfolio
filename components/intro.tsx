1bded64f3de2d6b51ba1addf8d4f19619fe69045
paragborkar <paragborkar200@gmail.com>
2023-12-25 15:39:27 +0530

    Intro Section Created

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,6 +5,10 @@ import React from 'react';
 import profile from '@/public/profile.jpeg';
 import { TypeAnimation } from 'react-type-animation';
 import {motion} from 'framer-motion';
+import Link from 'next/link';
+import {BsArrowRight, BsLinkedin} from 'react-icons/bs';
+import {HiDownload} from 'react-icons/hi';
+import { FaGithubSquare } from 'react-icons/fa';
 
 
 const Intro = () => {
@@ -13,6 +17,9 @@ const Intro = () => {
         <motion.div
         initial={{opacity:0 , scale:0}}
         animate={{opacity:1 , scale:1}}
+        transition={{
+            delay: 0.3,
+          }}
         className='flex items-center justify-center w-full'>
             <div className='flex items-center justify-center flex-col pt-[6rem]'>
                 <Image  src={profile} alt='Profile' width={192} height={192} quality={100} priority={true} 
@@ -30,11 +37,26 @@ const Intro = () => {
       ]}
       wrapper="span"
       speed={50}
-      style={{ fontSize: '2em', display: 'inline-block' }}
+      style={{ fontSize: '1.8rem', display: 'inline-block' }}
       repeat={Infinity}
-      className=' font-bold text-gray-700'
+      className=' font-bold bg-gradient-to-r from-gray-500 to-gray-950 text-transparent bg-clip-text h-[5rem] px-[3.5rem] sm:p-[initial] sm:h-[initial]'
     />
             </div>
+        </motion.div>
+        <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.5,
+        }}
+        className='mt-4 flex justify-center items-center flex-col gap-4 sm:flex-row sm:pt-[initial] sm:flex sm:gap-2
+        '>
+            <Link href='#contact'
+            className='bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full hover:scale-105'
+            >Contact Me<BsArrowRight/></Link>
+            <a className='bg-gray-500 px-7 py-3 flex items-center gap-2 rounded-full hover:scale-105' href='/CV.pdf' download>Download CV <HiDownload/></a>
+            <a className='bg-gray-400 p-4 text-gray-950 flex items-center gap-2 rounded-full hover:scale-105' href='https://www.linkedin.com/in/parag-borkar-34b98724b/' target='_blank'><BsLinkedin/></a>
+            <a className='bg-gray-400 p-4 text-gray-950 flex items-center gap-2 rounded-full hover:scale-105 'href='https://github.com/paragborkar' target='_blank'><FaGithubSquare/></a>
         </motion.div>
     </section>
   )
